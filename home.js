d70d6ad9362ca3befbe91e5763418f0c3fe71a40
Takis <panagiotisfoi@outlook.com>
2021-01-13 21:21:47 +0200

    Project 5
    wtf with the button ... htan ilithia dyskolo kathws den kserw pws na krataw plirofories sth mnhmh

--- home.js
+++ home.js
@@ -1,2 +1,30 @@
-    var Likes = 5;
-    document.getElementById("likenumber").innerHTML = Likes;
+function getLikes()
+{
+    var Likes = addLikes();
+    document.getElementById("likenumber").innerHTML = Likes++;
+}
+
+function addLikes()
+{
+    if (StorageEvent.clickcount) {
+        StorageEvent.clickcount = Number(StorageEvent.clickcount)+1;
+      } else {
+        StorageEvent.clickcount = 1;
+      }
+    a = 4 + StorageEvent.clickcount;
+    return a;
+}
+    
+function getPlayers()
+{
+    var Players = [
+        "Gioka",
+        "Giaprakas",
+        "Lomedil"
+        ];
+    
+    document.getElementById("playername1").innerHTML = Players[0];
+    document.getElementById("playername2").innerHTML = Players[1];
+    document.getElementById("playername3").innerHTML = Players[2];
+}
+      
